1f5038e1eddb2c190a4e7591575b9b2ead979aa0
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-03-11 17:58:45 -0400

    added like post feature

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import "../../styles/PostCard.css";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ModeCommentIcon from "@mui/icons-material/ModeComment";
@@ -8,7 +8,28 @@ import ModeCommentOutlinedIcon from "@mui/icons-material/ModeCommentOutlined";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 
 const PostCard = ({ post }) => {
-  console.log(post.user);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLikeClick = async () => {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+    console.log(user);
+    const response = await fetch("http://localhost:5000/posts/like", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        postId: post._id,
+        userId: user._id,
+      }),
+    });
+
+    if (response.ok) {
+      setIsLiked(!isLiked);
+    } else {
+      console.error("An error occurred while liking the post");
+    }
+  };
 
   return (
     <div className="post-card">
@@ -25,8 +46,12 @@ const PostCard = ({ post }) => {
       <div className="post-footer">
         <div className="post-actions">
           <div className="main-post-actions">
-            <div>
-              <FavoriteBorderIcon sx={{ fontSize: "2rem" }} />
+            <div className="like-button" onClick={handleLikeClick}>
+              {isLiked ? (
+                <FavoriteIcon sx={{ fontSize: "2rem" }} />
+              ) : (
+                <FavoriteBorderIcon sx={{ fontSize: "2rem" }} />
+              )}
             </div>
             <div>
               <ModeCommentOutlinedIcon sx={{ fontSize: "2rem" }} />
