4f44cc8cc1a5932b8297c77c8d28bd7bfcef8a88
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-03-27 19:40:55 -0400

    created change photo section for edit profile component

--- src/containers/EditProfile/EditProfile.jsx
+++ src/containers/EditProfile/EditProfile.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import "../../styles/EditProfile.css";
 
 const EditProfile = () => {
-  return <div>EditProfile</div>;
+  const user = JSON.parse(sessionStorage.getItem("user"));
+
+  return (
+    <div className="edit-profile-wrapper">
+      <div className="edit-profile-container">
+        <h2>Edit Profile</h2>
+        <div className="edit-picture-form">
+          <div className="edit-profile-picture-info">
+            <div className="profile-picture">
+              <img
+                src={user.profilePic}
+                alt="Profile"
+                className="profile-picture"
+              />
+            </div>
+            <div className="name-container">
+              <span className="edit-user-username-header">{user.username}</span>
+              <span>{user.fullName}</span>
+            </div>
+          </div>
+          <button className="edit-picture-button" type="submit">
+            Change Photo
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EditProfile;
