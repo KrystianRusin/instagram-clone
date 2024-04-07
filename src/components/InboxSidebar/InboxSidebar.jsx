4b650c04e5fdbec848e9ad0076455e25838344b7
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-04-06 21:08:50 -0400

    added inbox sidebar header

--- src/components/InboxSidebar/InboxSidebar.jsx
+++ src/components/InboxSidebar/InboxSidebar.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+import "./InboxSidebar.css";
+
 const InboxSidebar = () => {
+  const [user, setUser] = useState(null);
+
+  //Get the user from session storage
+  useEffect(() => {
+    setUser(JSON.parse(sessionStorage.getItem("user")));
+  }, []);
+
+  //TODO: Create chat list item component
+  //TODO: Create endpoint to retrieve all chats the user is a part of
+
+  //TODO: Create actual messaging system
+
   return (
     <div className="inbox-sidebar-container">
-      <div className="inbox-sidebar-header">InboxSidebar</div>
+      <div className="inbox-sidebar-header">
+        <div className="inbox-sidebar-header-actions">
+          <span className="inbox-sidebar-username">{user.username}</span>
+          <button>New Message</button>
+        </div>
+      </div>
+      <div>
+        <h1 className="messages-title">Messages</h1>
+      </div>
     </div>
   );
 };
