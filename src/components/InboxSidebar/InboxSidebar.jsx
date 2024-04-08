73f81029e22ae1f5ff12654310de4ac34086bb03
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-04-08 16:43:59 -0400

    added firestore database integration for real time chat

--- src/components/InboxSidebar/InboxSidebar.jsx
+++ src/components/InboxSidebar/InboxSidebar.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import db from "../../firebase";
 import "./InboxSidebar.css";
 
 const InboxSidebar = () => {
@@ -9,6 +12,21 @@ const InboxSidebar = () => {
     setUser(JSON.parse(sessionStorage.getItem("user")));
   }, []);
 
+  const handleNewChatClick = async () => {
+    const handleAddDoc = async () => {
+      try {
+        const docRef = await addDoc(collection(db, "myCollection"), {
+          test: "data",
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (error) {
+        console.error("Error adding document: ", error);
+      }
+    };
+
+    handleAddDoc();
+  };
+
   //TODO: Create chat list item component
   //TODO: Create endpoint to retrieve all chats the user is a part of
 
@@ -18,8 +36,10 @@ const InboxSidebar = () => {
     <div className="inbox-sidebar-container">
       <div className="inbox-sidebar-header">
         <div className="inbox-sidebar-header-actions">
-          <span className="inbox-sidebar-username">{user.username}</span>
-          <button>New Message</button>
+          <span className="inbox-sidebar-username">
+            {user ? user.username : "Loading...."}
+          </span>
+          <button onClick={handleNewChatClick}>New Message</button>
         </div>
       </div>
       <div>
